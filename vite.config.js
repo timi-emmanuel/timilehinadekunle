6cdf344e50dd974d4072f2778e48c3531f729ca7
timi-emmanuel <121458382+timi-emmanuel@users.noreply.github.com>
2025-12-21 13:22:33 +0100

    fix verel issues

--- vite.config.js
+++ vite.config.js
@@ -33,14 +33,7 @@ export default defineConfig({
     },
     chunkSizeWarningLimit: 1000,
     cssCodeSplit: true,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-        pure_funcs: ['console.log', 'console.info', 'console.debug'],
-      },
-    },
+    minify: 'esbuild',
     sourcemap: false,
   },
   optimizeDeps: {
